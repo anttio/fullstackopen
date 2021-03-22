d66440fe6f6476ffafada47894b84f828569df1f
Antti Onttonen <antti.onttonen@transferwise.com>
2021-03-22 10:07:13 +0200

    phonebook step 2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,12 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     const person = {
       name: newName,
     };
